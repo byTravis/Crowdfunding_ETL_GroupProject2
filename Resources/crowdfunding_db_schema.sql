5837e9fdbbf42ebd21d3215b0c41d8651d60cbff
Svarsha12 <varshasharma@Udyogs-MacBook-Pro.local>
2024-03-14 20:50:00 -0700

    added sql file

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,14 +1,40 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+﻿
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+DROP TABLE IF EXISTS contacts;
 
+
+CREATE TABLE "contacts" (
+    "contact_id" INT PRIMARY KEY  NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL
+  
+);
+
+
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10) PRIMARY KEY   NOT NULL,
+    "category" VARCHAR(50)   NOT NULL
+    
+     
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(50) PRIMARY KEY  NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL
+ 
+);
 
 CREATE TABLE "campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
+    "cf_id" INT PRIMARY KEY NOT NULL,
+    "contact_id" INT REFERENCES contacts("contact_id")  NOT NULL,
     "company_name" VARCHAR(144)   NOT NULL,
     "description" VARCHAR(144)   NOT NULL,
-    "goal" VARCHAR(50)   NOT NULL,
-    "pledged" INT   NOT NULL,
+    "goal" REAL   NOT NULL,
+    "pledged" REAL  NOT NULL,
     "outcome" VARCHAR(50)   NOT NULL,
     "backerr_count" INT   NOT NULL,
     "country" VARCHAR(10)   NOT NULL,
@@ -17,45 +43,13 @@ CREATE TABLE "campaign" (
     "end_date" DATE   NOT NULL,
     "staff_pick" BOOlEAN   NOT NULL,
     "spotlight" BOOLEAN   NOT NULL,
-    "category_id" VARCHAR(50)   NOT NULL,
-    "subcategory_id" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
+    "category_id" VARCHAR(50) REFERENCES category("category_id") NOT NULL,
+    "subcategory_id" VARCHAR(50)  REFERENCES subcategory("subcategory_id") NOT NULL
+	
 );
 
-CREATE TABLE "category" (
-    "category_id" VARCHAR(10)   NOT NULL,
-    "category" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id"
-     )
-);
-
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(50)   NOT NULL,
-    "subcategory" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
-);
-
-CREATE TABLE "contacts" (
-    "contact_id" INT   NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
-     )
-);
-
-ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
-REFERENCES "campaign" ("category_id");
-
-ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "campaign" ("subcategory_id");
-
-ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "campaign" ("contact_id");
+SELECT * FROM campaign;
+SELECT * FROM contacts;
+SELECT * FROM category;
+SELECT * FROM subcategory;
 
